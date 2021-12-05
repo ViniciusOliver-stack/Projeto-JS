44cc8720dc40cbecf554aef607352e990163b679
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-12-05 11:26:56 -0300

    Create index.js

---
+++ assets/pages/Countdown/js/index.js
@@ -0,0 +1,17 @@
+import Countdown from "./countdown.js";
+
+const tempoEvent = new Countdown("01 January 2022 00:00:00 GMT-0300")
+
+
+const tempos = document.querySelectorAll('[data-time]')
+
+function mostrarTempo(){
+    tempos.forEach((tempo, index) =>{
+        tempo.innerHTML = tempoEvent.total[index]
+    })
+}
+
+mostrarTempo()
+setInterval(mostrarTempo, 1000)
+
+
