dd0938961895aef94481ab66e6f05a5bdacf912e
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-12-05 11:25:56 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.bio', { delay: 500 });
+ScrollReveal().reveal('.projects', { delay: 400 });
+ScrollReveal().reveal('.card-text', { delay: 450 });
